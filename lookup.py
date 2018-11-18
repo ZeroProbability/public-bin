532a658789aacd8f0b9215a5e18c1ad2b0d908ad
Anbu Ramasamy <git@anbu.work>
2018-11-18 18:28:48 +0000

    python

--- lookup.py
+++ lookup.py
@@ -6,10 +6,11 @@ def tags_match(small_list, full_list):
 
 def decompose_line(line):
     tags, desc, command = line.split(':', 2)
+    tags, desc, command = tags.strip(), desc.strip(), command.strip() 
     tags = [i.strip() for i in tags.split(',')]
     return {'tags': tags, 'description': desc, 'command': command}
 
 # ------------------
 
 def test_decompose_line():
-    assert decompose_line('a1, a2:b:c:d:e') == {'tags': ['a1', 'a2'], 'description': 'b', 'command': 'c:d:e'}
+    assert decompose_line('a1, a2:  b   :   c:d:e') == {'tags': ['a1', 'a2'], 'description': 'b', 'command': 'c:d:e'}
