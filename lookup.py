b834751a7b62b3ca4da7be4b8a055641a11e19e1
Anbu Ramasamy <git@anbu.work>
2018-11-18 17:28:09 +0000

    migrating to python

---
+++ lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def tags_match(small_list, full_list):
+    return set(small_list) < set(full_list)
+
+def decompose_line(line):
+    tags, desc, command = line.split(':', 2)
+    tags = [i.strip() for i in tags.split(',')]
+    return {'tags': tags, 'description': desc, 'command': command}
+
+# ------------------
+
+def test_decompose_line():
+    assert decompose_line('a1, a2:b:c:d:e') == {'tags': ['a1', 'a2'], 'description': 'b', 'command': 'c:d:e'}
